30fce398cc07d6f89618864327c303f1ae49eadf
Cassandra Coal <cassie.coal@gmail.com>
2024-12-10 21:27:48 -0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+// import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>HEY A HEADER MIGHT GO HERE</header>
+  <br/>
+
+  <RouterView />
+</template>
+
+<style scoped>
+</style>
---
+++ src/views/GalleryView.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="about">
+      <h1>Image Gallery</h1>
+      <br/>
+      <div class="grid-container">
+        <!-- FOR EACH IMAGE, GRID ITEM -->
+      </div>
+    </div>
+</template>
+  
+<script>
+</script>
+
+<style>
+  /* @media (min-width: 1024px) {
+    .about {
+      min-height: 100vh;
+      display: flex;
+      align-items: center;
+    }
+  } */
+</style>
+  
