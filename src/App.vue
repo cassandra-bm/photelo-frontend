bd7fb48475770d4e8a37e024db167928ef143385
Cassandra Coal <cassie.coal@gmail.com>
2024-12-18 19:34:00 -0600

    Comment out base css, create navbar and header in app root

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,42 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>HEY A HEADER MIGHT GO HERE</header>
-  <br/>
+  <header>
+    <h1>
+      photelo
+    </h1>
+    <div id="navbar">
+      <p>
+        <a href="/gallery">gallery</a> | 
+        <a href="/submission">submit</a> | 
+        <a href="/voting">vote</a>
+      </p>
+    </div>
+    
+  </header>
+  <div id="nav-border"/>
 
   <RouterView />
 </template>
 
 <style scoped>
+#navbar {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  font-size: 20px;
+}
+
+#nav-border {
+  width: 100%;
+  border: 1px solid black;
+}
+
+h1 {
+  /* margin: auto; */
+  margin: 2% 5%;
+  text-align: center;
+}
+
+
 </style>
