a80c82b5ff5cb4cf8e3d1a8bde2ea64321e429f7
Cassandra Coal <cassie.coal@gmail.com>
2024-12-18 19:34:25 -0600

    Add views to router

--- src/router/index.js
+++ src/router/index.js
@@ -1,33 +1,30 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
 import GalleryView from '../views/GalleryView.vue'
 import Ping from '@/components/Ping.vue'
+import SubmissionView from '@/views/SubmissionView.vue'
+import VotingView from '@/views/VotingView.vue'
 
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: HomeView,
-    // },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import('../views/AboutView.vue'),
-    // },
     {
       path: '/ping',
       name: 'ping',
       component: Ping
     },
     {path: '/gallery',
-      name: 'gallery',
+      name: 'galleryview',
       component: GalleryView
+    },
+    {path: '/submission',
+      name: 'submissionview',
+      component: SubmissionView
+    },
+    {path: '/voting',
+      name: 'votingview',
+      component: VotingView
     }
   ],
 })
